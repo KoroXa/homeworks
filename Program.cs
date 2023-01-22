41d0440581fd668c8e01d86f88f43903b061ce79
KoroXa <113926244+KoroXa@users.noreply.github.com>
2023-01-22 11:44:52 +0300

    Calculator

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿double A, B;
+A = double.Parse(Console.ReadLine());
+string C = Console.ReadLine();
+B = double.Parse(Console.ReadLine());
+
+if (C == "+")
+{
+    Console.WriteLine(A + B);
+}
+if (C == "-")
+{
+    Console.WriteLine(A - B);
+}
+if (C == "/")
+{
+    Console.WriteLine(A / B);
+}
+if (C == "*")
+{
+    Console.WriteLine(A * B);
+}
